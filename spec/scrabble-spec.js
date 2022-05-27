2dd618e10c80d4f5797e267d2132c9e9e9351367
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-27 22:51:07 +0100

    one extension completed

--- spec/scrabble-spec.js
+++ spec/scrabble-spec.js
@@ -1,54 +1,72 @@
 // Note: you shouldn't need to change anything in this file.
 
-Scrabble = require('../src/scrabble')
+Scrabble = require('../src/scrabble');
 let scrabble;
 
-describe("Scrabble", function() {
-  it('returns 0 for empty words', function() {
-    scrabble = new Scrabble('')
+describe('Scrabble', function () {
+  it('returns 0 for empty words', function () {
+    scrabble = new Scrabble('');
 
-    expect(scrabble.score()).toEqual(0)
-  })
+    expect(scrabble.score()).toEqual(0);
+  });
 
-  it('returns 0 for whitespace', function() {
-    scrabble = new Scrabble(" \t\n")
+  it('returns 0 for whitespace', function () {
+    scrabble = new Scrabble(' \t\n');
 
-    expect(scrabble.score()).toEqual(0)
-  })
+    expect(scrabble.score()).toEqual(0);
+  });
 
-  it('returns 0 for null', function() {
-    scrabble = new Scrabble(null)
+  it('returns 0 for null', function () {
+    scrabble = new Scrabble(null);
 
-    expect(scrabble.score()).toEqual(0)
-  })
+    expect(scrabble.score()).toEqual(0);
+  });
 
-  it('scores short word', function() {
-    scrabble = new Scrabble('a')
+  it('scores short word', function () {
+    scrabble = new Scrabble('a');
 
-    expect(scrabble.score()).toEqual(1)
-  })
+    expect(scrabble.score()).toEqual(1);
+  });
 
-  it('scores short word', function() {
-    scrabble = new Scrabble('f')
+  it('scores short word', function () {
+    scrabble = new Scrabble('f');
 
-    expect(scrabble.score()).toEqual(4)
-  })
+    expect(scrabble.score()).toEqual(4);
+  });
 
-  it('scores a simple word', function() {
-    scrabble = new Scrabble('street')
+  it('scores a simple word', function () {
+    scrabble = new Scrabble('street');
 
-    expect(scrabble.score()).toEqual(6)
-  })
+    expect(scrabble.score()).toEqual(6);
+  });
 
-  it('scores a more complicated word', function() {
-    scrabble = new Scrabble('quirky')
+  it('scores a more complicated word', function () {
+    scrabble = new Scrabble('quirky');
 
-    expect(scrabble.score()).toEqual(22)
-  })
+    expect(scrabble.score()).toEqual(22);
+  });
 
-  it('scores a case-insensitive word', function() {
-    scrabble = new Scrabble('OXYPHENBUTAZONE')
+  it('scores a case-insensitive word', function () {
+    scrabble = new Scrabble('OXYPHENBUTAZONE');
 
-    expect(scrabble.score()).toEqual(41)
-  })
-})
+    expect(scrabble.score()).toEqual(41);
+  });
+
+  it('double letter', function () {
+    scrabble = new Scrabble('OXYPH[E]NBUTA[Z]ONE');
+
+    expect(scrabble.score()).toEqual(52);
+  });
+
+  it('triple letter', function () {
+    scrabble = new Scrabble('O{X}YPH{E}NBUTA{Z}ONE');
+
+    expect(scrabble.score()).toEqual(79);
+  });
+
+  it('triple and double letters', function () {
+    scrabble = new Scrabble('O{X}Y[P]H{E}NB[U]TA{Z}ONE');
+
+    expect(scrabble.score()).toEqual(83);
+  });
+});
--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,53 @@
-class Scrabble {
-  // Write your implementation here
+// take inputted word
 
+const letterValues = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
+};
+
+class Scrabble {
+  constructor(word) {
+    this.word = [];
+    if (word != null) this.word.push(word.toUpperCase());
+  }
+  score() {
+    let sum = 0;
+    const letterMatch = Object.entries(letterValues);
+    letterMatch.map((x) =>
+      this.word.map((word) => {
+        for (let i = 0; i < word.length; i++) {
+          if (word[i].includes(x[0])) sum += x[1];
+          if (word[i] === '[' && word[i + 1].includes(x[0])) sum += x[1];
+          if (word[i] === '{' && word[i + 1].includes(x[0])) sum += x[1] * 2;
+        }
+      })
+    );
+    return sum;
+  }
 }
 
-module.exports = Scrabble
+module.exports = Scrabble;
